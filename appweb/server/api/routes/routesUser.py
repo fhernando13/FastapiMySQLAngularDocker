8523a34d06384d9a64b69ab3507505613e04eb8d
fhers <fhers667@gmail.com>
2023-05-15 16:53:50 -0600

    finished operations

--- appweb/server/api/routes/routesUser.py
+++ appweb/server/api/routes/routesUser.py
@@ -1,45 +1,62 @@
 from fastapi import APIRouter
-from config.conexion import conn
+from config.conexion import conn, engine
 from models.modelUser import users
 from schemas.schemaUser import userSchema
-import json
+from fastapi.responses import Response
+from fastapi import FastAPI, HTTPException
+from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
+
 
 user = APIRouter()
 
 
-@user.get('/index')
+@user.get('/api/index')
 def index():
     return "Welcome to the FastAPI"
 
 
 @user.get('/api/allUsers')
-def get_users():        
-    results = conn.execute(users.select()).fetchall()
-    return results
+def get_users():      
+    with engine.connect() as condb:  
+        results = condb.execute(users.select()).fetchall()
+        condb.close()
+        return results
+        
           
-          
-@user.get('/user')
-def get_user():
-    return "only one user"
+@user.get('/api/one_user/{id}', response_model=userSchema)
+def get_user(id: str):
+    with engine.connect() as condb:
+        result = condb.execute(users.select().where(users.c.id == id)).first()
+        if result == None:
+            raise HTTPException(status_code=404, detail="User not exist")
+        return result
+     
 
-
-@user.post('/add_user', status_code=201)
+@user.post('/api/add_user')
 def add_user(user: userSchema):
-    try:
+    with engine.connect() as condb:
         new_user = {"name":user.name, "lastname":user.lastname, "email":user.email, "age":user.age}
-        conn.execute(users.insert().values(new_user))
-        conn.commit()
+        condb.execute(users.insert().values(new_user))
         return "add user"
-    except Exception as e:
-        return print(f'Error: {e}', status_code=400) 
 
 
-@user.put('/update_user')
-def update_user():
-    return "update an user"
+@user.put('/api/update_user/{id}')
+def update_user(data_update: userSchema, id: str):
+    with engine.connect() as condb:
+        sql = condb.execute(users.select().where(users.c.id == id)).first()
+        if sql == None:
+            raise HTTPException(status_code=404, detail="User not exist")
+        condb.execute(users.update().values(name=data_update.name, 
+                                             lastname=data_update.lastname, 
+                                             email=data_update.email, 
+                                             age=data_update.age).where(users.c.id == id))
+        result = condb.execute(users.select().where(users.c.id == id)).first()
+        print("User updated")
+        return result
 
 
-@user.delete('/delete_user')
-def delete_user():
-    return "delete an user"
-
+@user.delete('/api/delete_user/{id}', status_code=HTTP_204_NO_CONTENT)
+def delete_user(id: str):
+    with engine.connect() as condb:
+        condb.execute(users.delete().where(users.c.id == id))
+        return Response(status_code=HTTP_204_NO_CONTENT)
