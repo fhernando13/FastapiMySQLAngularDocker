cd507278628dcab9727c0df896ffed21ac44851f
fhers <fhers667@gmail.com>
2023-05-21 23:03:15 -0600

    front login

--- appweb/client/login/src/app/services/login.service.ts
+++ appweb/client/login/src/app/services/login.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Login } from '../models/login';
-import { environment } from 'src/environments/environment.prod';;
+import { environment } from 'src/environments/environment.prod';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,14 @@ export class LoginService {
 
   API_URI = environment.localHost;
 
-  constructor(private http: HttpClient) 
-  { 
+  constructor(private http: HttpClient) {}
 
+  login(logged: Login): Observable<any>{
+    return this.http.post(this.API_URI+'login', logged);
   }
 
-  login(logged: Login){
-    return this.http.post(this.API_URI+'login', logged);
+  logout(): void{
+    
   }
   
 }
